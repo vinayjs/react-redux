8a381bdc62f1fd897e24994597a8a03feaec6145
vinay chandran <vinay.sarvodaya1991@gmail.com>
2023-11-15 11:11:08 +0530

    use  createSlice, useSelector

--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import userReducer from './features.js/user'
 
+
+const store = configureStore({
+  reducer:{
+    user: userReducer
+  }
+})
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
+    
   </React.StrictMode>
 );
 
